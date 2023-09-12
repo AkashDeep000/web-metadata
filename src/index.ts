62acadb9eafaaea810fc84c66025b8562511630b
AkashDeep000 <76022812+AkashDeep000@users.noreply.github.com>
2023-09-12 17:02:07 +0000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import puppeteer from "puppeteer-core"
+
+const app = new Hono()
+let browser; 
+(async() => {
+  browser = await puppeteer.launch({
+       headless: true,
+       //product: "chromium",
+       executablePath: '/nix/store/v099qxf96q4cm 47kskh9j146jfyh058i-chromium-108.0.5359. 94-sandbox/bin/__chromium-suid-sandbox',
+       args: [
+       "--no-sandbox",
+       "--disable-gpu",
+       ]
+   });
+ console.log("browser launched!")
+})()
+
+app.get('/', (c) => c.text('Hello Hono!'))
+
+serve(app)
